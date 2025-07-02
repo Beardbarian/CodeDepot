0c3cbc9ff2a9da917cf08da1fc5da4903e623bb0
Beardbarian <beardbarian@gmail.com>
2025-07-02 09:58:17 -0400

    Create Excel Protected View Disabled.bat

---
+++ Batch/Excel Protected View Disabled.bat
@@ -0,0 +1,3 @@
+reg add HKEY_CURRENT_USER\Software\Microsoft\Office\16.0\Excel\Security\ProtectedView /v DisableInternetFilesInPV /t REG_DWORD /d 1
+reg add HKEY_CURRENT_USER\Software\Microsoft\Office\16.0\Excel\Security\ProtectedView /v DisableAttachmentsInPV /t REG_DWORD /d 1
+reg add HKEY_CURRENT_USER\Software\Microsoft\Office\16.0\Excel\Security\ProtectedView /v DisableUnsafeLocationsInPV /t REG_DWORD /d 1
