0e17f5b919766762574ddc483df478f6e73d1480
Beardbarian <beardbarian@gmail.com>
2025-07-15 14:14:33 -0400

    Update WMIC Fix.bat

--- Batch/WMIC Fix.bat
+++ Batch/WMIC Fix.bat
@@ -1,15 +1,15 @@
 :: Clears WMI configuration so it can be rebuilt. Useful for systems that aren't reporting their configuration.
 :: For after script testing: https://www.lisenet.com/2014/get-windows-system-information-via-wmi-command-line-wmic/
 
-:: Disable and stop winmgmt service
+write-Host "Disabling and stoping winmgmt service" -ForegroundColor Green  
 sc config winmgmt start= disabled
 net stop winmgmt /y
-:: Clear repository
+write-Host "Clearing repository" -ForegroundColor Green  
 del C:\Windows\System32\wbem\repository\* /Q /F /S
-:: Enable and start the winmgmt service
+write-Host "Enabling and starting the winmgmt service" -ForegroundColor Green  
 sc config winmgmt start= auto
 net start winmgmt
-:: Cleanup files
+write-Host "Cleaning up files" -ForegroundColor Green  
 cd C:\Windows\System32\wbem\
 for /f %s in ('dir /b *.mof') do mofcomp %s
 for /f %s in ('dir /b en-us\*.mfl') do mofcomp en-us\%s
