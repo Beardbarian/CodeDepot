b563d68a1fbecc5b93baeafa19b77abc342f3c27
Beardbarian <beardbarian@gmail.com>
2025-07-15 14:07:49 -0400

    Update Profile Rebuild.bat

--- Batch/Profile Rebuild.bat
+++ Batch/Profile Rebuild.bat
@@ -1,7 +1,7 @@
 robocopy C:\Users\%username% C:\Users\%username%.old /E /Z /ZB /eta /np /v /XC /XN /xjd /xjf /XO /R:3 /W:0 /XD "C:\Users\%username%\Appdata\"
 
-#Alternatively, login as local or domain admin, rename user folder to "user.old" and then delete profile. Use the following script to migrate back data.
-#attrib is there for instances where the user folder becomes hidden due to robocopy
+:: Alternatively, login as local or domain admin, rename user folder to "user.old" and then delete profile. Use the following script to migrate back data.
+:: attrib is there for instances where the user folder becomes hidden due to robocopy
 takown /f C:\%username%.old /r /d y
 robocopy C:\Users\%username%.old C:\Users\%username% /E /Z /ZB /eta /np /v /XC /XN /xjd /xjf /XO /R:3 /W:0 /XD "C:\Users\%username%.old\Appdata\"
 attrib -h  -s  -a C:\Users\%username%
