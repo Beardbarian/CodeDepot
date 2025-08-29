ff553a631d7fa7013402293c24a8bf004aaf47b8
Beardbarian <beardbarian@gmail.com>
2025-08-29 09:40:21 -0400

    Update Config Manager Refresh.ps1

--- Powershell/Config Manager Refresh.ps1
+++ Powershell/Config Manager Refresh.ps1
@@ -18,3 +18,25 @@ WMIC /namespace:\\root\ccm path sms_client CALL TriggerSchedule "{00000000-0000-
 WMIC /namespace:\\root\ccm path sms_client CALL TriggerSchedule "{00000000-0000-0000-0000-000000000027}" /NOINTERACTIVE
 #Windows Installers Source List Update Cycle
 WMIC /namespace:\\root\ccm path sms_client CALL TriggerSchedule "{00000000-0000-0000-0000-000000000032}" /NOINTERACTIVE
+
+
+#2nd attempt WIP
+-----------------------------------------------
+$ComputerName = $env:COMPUTERNAME # Pulls PC Name, can replace with the target computer's name to run remotely
+
+$ActionGUIDs = @(
+    "{00000000-0000-0000-0000-000000000121}", # Application Deployment Evaluation Cycle
+    "{00000000-0000-0000-0000-000000000003}", # Discovery Data Collection Cycle
+    "{00000000-0000-0000-0000-000000000001}", # Hardware Inventory Cycle
+    "{00000000-0000-0000-0000-000000000022}", # Machine Policy Retrieval & Evaluation Cycle
+    "{00000000-0000-0000-0000-000000000031}", # Software Metering Usage Report Cycle
+    "{00000000-0000-0000-0000-000000000108}", # Software Updates Deployment Evaluation Cycle
+    "{00000000-0000-0000-0000-000000000113}", # Software Updates Scan Cycle
+    "{00000000-0000-0000-0000-000000000021}", # User Policy Retrieval & Evaluation Cycle
+    "{00000000-0000-0000-0000-000000000032}"  # Windows Installer Source List Update Cycle
+)
+
+foreach ($guid in $ActionGUIDs) {
+    Write-Host "Triggering action with GUID: $guid on $ComputerName"
+    Invoke-WmiMethod -Namespace "root\ccm" -Class "SMS_Client" -Name "TriggerSchedule" -ArgumentList $guid -ComputerName $ComputerName
+}
