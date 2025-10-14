5f8953cb95b2eed5e456f9b38a96e8bf9c906263
Beardbarian <beardbarian@gmail.com>
2025-10-14 11:50:33 -0400

    Update MDM enrollment URLs in PowerShell script

--- Powershell/Intune AutoEnroll Trigger.ps1
+++ Powershell/Intune AutoEnroll Trigger.ps1
@@ -1,12 +1,13 @@
-$key = 'SYSTEM\CurrentControlSet\Control\CloudDomainJoin\TenantInfo\*'
-$keyinfo = Get-Item "HKLM:\$key"
-$url = $keyinfo.name
-$url = $url.Split("\")[-1]
-$path = "HKLM:\SYSTEM\CurrentControlSet\Control\CloudDomainJoin\TenantInfo\\$url"
+# Set MDM Enrollment URLs 
+$key = 'SYSTEM\CurrentControlSet\Control\CloudDomainJoin\TenantInfo\*' 
+$keyinfo = Get-Item "HKLM:\$key" 
+$url = $keyinfo.name 
+$url = $url.Split("\")[-1] 
+$path = "HKLM:\SYSTEM\CurrentControlSet\Control\CloudDomainJoin\TenantInfo\$url"
 
-New-ItemProperty -LiteralPath $path -Name 'MdmEnrollmentUrl' -Value 'https://enrollment.manage.microsoft.com/enrollmentserver/discovery.svc' -PropertyType String -Force -ea SilentlyContinue;
-New-ItemProperty -LiteralPath $path  -Name 'MdmTermsOfUseUrl' -Value 'https://portal.manage.microsoft.com/TermsofUse.aspx' -PropertyType String -Force -ea SilentlyContinue;
-New-ItemProperty -LiteralPath $path -Name 'MdmComplianceUrl' -Value 'https://portal.manage.microsoft.com/?portalAction=Compliance' -PropertyType String -Force -ea SilentlyContinue;
+New-ItemProperty -LiteralPath $path -Name 'MdmEnrollmentUrl' -Value 'https://enrollment.manage.microsoft.com/enrollmentserver/discovery.svc' -PropertyType String -Force -ea SilentlyContinue; 
+New-ItemProperty -LiteralPath $path -Name 'MdmTermsOfUseUrl' -Value 'https://portal.manage.microsoft.com/TermsofUse.aspx' -PropertyType String -Force -ea SilentlyContinue; 
+New-ItemProperty -LiteralPath $path -Name 'MdmComplianceUrl' -Value 'https://portal.manage.microsoft.com/?portalAction=Compliance' -PropertyType String -Force -ea SilentlyContinue; 
 
-# Trigger AutoEnroll
+# Trigger AutoEnroll 
 C:\Windows\system32\deviceenroller.exe /c /AutoEnrollMDM
