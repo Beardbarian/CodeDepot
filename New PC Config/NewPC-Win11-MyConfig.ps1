1d388050daf8d4564c1a1d1997743dc27816e316
Beardbarian <beardbarian@gmail.com>
2026-03-26 16:23:32 -0400

    Update NewPC-Win11-MyConfig.ps1
    Rewrote to better handling of apps.
    Updated structure for better management and improved logic.

--- New PC Config/NewPC-Win11-MyConfig.ps1
+++ New PC Config/NewPC-Win11-MyConfig.ps1
@@ -1,49 +1,79 @@
-﻿#Remove Apps
-Get-AppxPackage -AllUsers *Clipchamp.Clipchamp* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *bing* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Microsoft.GetStarted* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Microsoft.Messaging* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Microsoft.Microsoft3DViewer* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Microsoft.MicrosoftOfficeHub* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Microsoft.MicrosoftSolitaireCollection* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Microsoft.MixedReality.Portal* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Microsoft.News* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Microsoft.OneConnect* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Microsoft.People* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Microsoft.PowerAutomateDesktop* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Microsoft.SkypeApp* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *microsoft.windowscommunicationsapps* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Microsoft.WindowsFeedbackHub* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Microsoft.WindowsMaps* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Microsoft.YourPhone* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Microsoft.ZuneMusic* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Microsoft.ZuneVideo* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *MicrosoftCorporationII.MicrosoftFamily* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Microsoft.OutlookForWindows* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Microsoft.Todos* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *MSTeams* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Copilot* | Remove-AppxPackage
-#Disables Windows Spotlight, sets background to solid color black.
-Set-ItemProperty -Path 'HKCU:\Software\Policies\Microsoft\Windows\CloudContent' -Name DisableSpotlightCollectionOnDesktop -type DWord -Value '1'
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\DesktopSpotlight\Settings' -Name EnabledState -type DWord -Value '0'
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Wallpapers' -Name BackgroundType -type DWord -Value '1'
-Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name WallPaper -Value ''
-Set-ItemProperty -Path 'HKCU:\Control Panel\Colors' -Name Background -Value '0 0 0'
-#Left align taskbar
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name TaskbarAl -type DWord -Value '0'
-#Set dark mode theme
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize' -Name AppsUseLightTheme -type DWord -Value '0'
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize' -Name SystemUsesLightTheme -type DWord -Value '0'
-#Hide search
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Search' -Name SearchboxTaskbarMode -type DWord -Value '0'
-#Hide taskview
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name ShowTaskViewButton -type DWord -Value '0'
-#Hide chat
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name TaskbarMn -type DWord -Value '0'
-#Disable widgets
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\PolicyManager\default\NewsAndInterests\AllowNewsAndInterests' -Name value -type DWord -Value '0'
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Dsh' -Name AllowNewsAndInterests -type DWord -Value '0'
-#Use old context menu
-New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Value "" -Force
-# Restart windows Explorer
-Stop-Process -ProcessName explorer
+# 1. Provisioned App Removal
+$AppsToRemove = @(
+    "*Clipchamp.Clipchamp*", 
+    "*bing*", 
+    "*Microsoft.GetStarted*", 
+    "*Microsoft.Messaging*",
+    "*Microsoft.Microsoft3DViewer*", 
+    "*Microsoft.MicrosoftOfficeHub*", 
+    "*Microsoft.MicrosoftSolitaireCollection*", 
+    "*Microsoft.MixedReality.Portal*",
+    "*Microsoft.News*", 
+    "*Microsoft.OneConnect*", 
+    "*Microsoft.People*",
+    "*Microsoft.PowerAutomateDesktop*", 
+    "*Microsoft.SkypeApp*",
+    "*microsoft.windowscommunicationsapps*", 
+    "*Microsoft.WindowsFeedbackHub*",
+    "*Microsoft.WindowsMaps*", 
+    "*Microsoft.YourPhone*", 
+    "*Microsoft.ZuneMusic*",
+    "*Microsoft.ZuneVideo*", 
+    "*MicrosoftCorporationII.MicrosoftFamily*",
+    "*Microsoft.OutlookForWindows*", 
+    "*Microsoft.Todos*", 
+    "*MSTeams*", 
+    "*Copilot*"
+)
+
+foreach ($AppName in $AppsToRemove) {
+    # Remove from current user session
+    Get-AppxPackage -Name $AppName -ErrorAction SilentlyContinue | Remove-AppxPackage -ErrorAction SilentlyContinue
+    # Remove from the Windows Image (Provisioned) so they don't come back
+    Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -like $AppName} | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
+}
+
+# 2. UI Customization (Left Align, Dark Mode, Solid Black)
+$RegistryPaths = @(
+    "HKCU:\Software\Policies\Microsoft\Windows\CloudContent",
+    "HKCU:\Software\Microsoft\Windows\CurrentVersion\DesktopSpotlight\Settings",
+    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Wallpapers",
+    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced",
+    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize",
+    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search"
+)
+
+# Ensure paths exist before setting values
+foreach ($path in $RegistryPaths) {
+    if (-not (Test-Path $path)) { New-Item -Path $path -Force | Out-Null }
+}
+
+# Apply Styles
+Set-ItemProperty -Path 'HKCU:\Software\Policies\Microsoft\Windows\CloudContent' -Name DisableSpotlightCollectionOnDesktop -Type DWord -Value 1
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\DesktopSpotlight\Settings' -Name EnabledState -Type DWord -Value 0
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Wallpapers' -Name BackgroundType -Type DWord -Value 1
+Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name WallPaper -Value ""
+Set-ItemProperty -Path 'HKCU:\Control Panel\Colors' -Name Background -Value "0 0 0"
+
+# Taskbar and Search
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name TaskbarAl -Type DWord -Value 0 # Left Align
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Search' -Name SearchboxTaskbarMode -Type DWord -Value 0 # Hide Search
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name ShowTaskViewButton -Type DWord -Value 0
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name TaskbarMn -Type DWord -Value 0
+
+# Dark Mode
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize' -Name AppsUseLightTheme -Type DWord -Value 0
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize' -Name SystemUsesLightTheme -Type DWord -Value 0
+
+# 3. System Tweaks (Widgets & Context Menu)
+# Disable Widgets
+$WidgetPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Dsh'
+if (-not (Test-Path $WidgetPath)) { New-Item -Path $WidgetPath -Force | Out-Null }
+Set-ItemProperty -Path $WidgetPath -Name AllowNewsAndInterests -Type DWord -Value 0
+
+# Restore Classic Context Menu (Win10 Style)
+$ContextKey = "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32"
+if (-not (Test-Path $ContextKey)) { New-Item -Path $ContextKey -Value "" -Force | Out-Null }
+
+# 4. Refresh Explorer
+Stop-Process -ProcessName explorer -Force
