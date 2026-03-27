774efac69307d004668f809bbb063e52a0b5be44
Beardbarian <beardbarian@gmail.com>
2026-03-26 21:12:35 -0400

    Update DesktopConfig.ps1

--- New PC Config/DesktopConfig.ps1
+++ New PC Config/DesktopConfig.ps1
@@ -1,50 +1,37 @@
 # 1. Create Detection Flag IMMEDIATELY (HKLM)
-# We do this first so Intune knows the script at least started.
-$registryPath = "HKLM:\SOFTWARE\MyCustomConfig"
-if (-not (Test-Path $registryPath)) {
-    New-Item -Path $registryPath -Force | Out-Null
-}
-New-ItemProperty -Path $registryPath -Name "ConfigApplied" -Value 1 -PropertyType DWord -Force
+# We use explicit 64-bit registry calls to ensure Intune (32-bit) doesn't redirect this to WOW6432Node
+$RegistryPath = "SOFTWARE\MyCustomConfig"
+$RegistryKey = [Microsoft.Win32.RegistryKey]::OpenBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, [Microsoft.Win32.RegistryView]::Registry64)
+$SubKey = $RegistryKey.CreateSubKey($RegistryPath)
+$SubKey.SetValue("ConfigApplied", 1, [Microsoft.Win32.RegistryValueKind]::DWord)
+$SubKey.Close()
+$RegistryKey.Close()
 
 # 2. Provisioned App Removal
 $AppsToRemove = @(
-    "*Clipchamp.Clipchamp*", 
-    "*bing*", 
-    "*Microsoft.GetStarted*", 
-    "*Microsoft.Messaging*",
-    "*Microsoft.Microsoft3DViewer*", 
-    "*Microsoft.MicrosoftOfficeHub*", 
-    "*Microsoft.MicrosoftSolitaireCollection*", 
-    "*Microsoft.MixedReality.Portal*",
-    "*Microsoft.News*", 
-    "*Microsoft.OneConnect*", 
-    "*Microsoft.People*",
-    "*Microsoft.PowerAutomateDesktop*", 
-    "*Microsoft.SkypeApp*",
-    "*microsoft.windowscommunicationsapps*", 
-    "*Microsoft.WindowsFeedbackHub*",
-    "*Microsoft.WindowsMaps*", 
-    "*Microsoft.YourPhone*", 
-    "*Microsoft.ZuneMusic*",
-    "*Microsoft.ZuneVideo*", 
-    "*MicrosoftCorporationII.MicrosoftFamily*",
-    "*Microsoft.OutlookForWindows*", 
-    "*Microsoft.Todos*", 
-    "*MSTeams*", 
-    "*Copilot*"
+    "*Clipchamp.Clipchamp*", "*bing*", "*Microsoft.GetStarted*", "*Microsoft.Messaging*",
+    "*Microsoft.Microsoft3DViewer*", "*Microsoft.MicrosoftOfficeHub*", 
+    "*Microsoft.MicrosoftSolitaireCollection*", "*Microsoft.MixedReality.Portal*",
+    "*Microsoft.News*", "*Microsoft.OneConnect*", "*Microsoft.People*",
+    "*Microsoft.PowerAutomateDesktop*", "*Microsoft.SkypeApp*",
+    "*microsoft.windowscommunicationsapps*", "*Microsoft.WindowsFeedbackHub*",
+    "*Microsoft.WindowsMaps*", "*Microsoft.YourPhone*", "*Microsoft.ZuneMusic*",
+    "*Microsoft.ZuneVideo*", "*MicrosoftCorporationII.MicrosoftFamily*",
+    "*Microsoft.OutlookForWindows*", "*Microsoft.Todos*", "*MSTeams*", "*Copilot*"
 )
 
 foreach ($AppName in $AppsToRemove) {
-    Get-AppxPackage -Name $AppName -ErrorAction SilentlyContinue | Remove-AppxPackage -ErrorAction SilentlyContinue
+    Get-AppxPackage -Name $AppName -AllUsers -ErrorAction SilentlyContinue | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue
     Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -like $AppName} | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
 }
 
-# 3. System-Wide Registry Tweaks
+# 3. System-Wide Registry Tweaks (Widgets)
 $WidgetPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Dsh'
 if (-not (Test-Path $WidgetPath)) { New-Item -Path $WidgetPath -Force | Out-Null }
 Set-ItemProperty -Path $WidgetPath -Name AllowNewsAndInterests -Type DWord -Value 0
 
-# 4. Active Setup for User-Specific UI Tweaks
+# 4. Active Setup for User-Specific UI Tweaks (Dark Mode, Black BG, Taskbar)
+# This stages the commands to run as the user upon next login
 $UserSettingsCmd = 'powershell.exe -ExecutionPolicy Bypass -Command "' +
     'Set-ItemProperty -Path \"HKCU:\Software\Policies\Microsoft\Windows\CloudContent\" -Name \"DisableSpotlightCollectionOnDesktop\" -Value 1 -Force -ErrorAction SilentlyContinue; ' +
     'Set-ItemProperty -Path \"HKCU:\Software\Microsoft\Windows\CurrentVersion\DesktopSpotlight\Settings\" -Name \"EnabledState\" -Value 0 -Force -ErrorAction SilentlyContinue; ' +
@@ -60,11 +47,16 @@ $UserSettingsCmd = 'powershell.exe -ExecutionPolicy Bypass -Command "' +
     'New-Item -Path \"HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32\" -Value \"\" -Force' +
 '"'
 
-# Register the Active Setup component in HKLM
-$ActiveSetupPath = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\MyCustomConfig"
-if (-not (Test-Path $ActiveSetupPath)) { New-Item -Path $ActiveSetupPath -Force | Out-Null }
-Set-ItemProperty -Path $ActiveSetupPath -Name "Version" -Value "1"
-Set-ItemProperty -Path $ActiveSetupPath -Name "StubPath" -Value $UserSettingsCmd
+# Register Active Setup in 64-bit hive
+$ASPath = "SOFTWARE\Microsoft\Active Setup\Installed Components\MyCustomConfig"
+$RegistryKey = [Microsoft.Win32.RegistryKey]::OpenBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, [Microsoft.Win32.RegistryView]::Registry64)
+$SubKey = $RegistryKey.CreateSubKey($ASPath)
+$SubKey.SetValue("Version", "1")
+$SubKey.SetValue("StubPath", $UserSettingsCmd)
+$SubKey.Close()
+$RegistryKey.Close()
 
-# 5. Exit with Soft Reboot Code
-exit 3010
+# 5. Forced Hard Reboot
+# Triggers a restart in 5 seconds to flush the E3 identity broker and trigger Active Setup
+Start-Process "shutdown.exe" -ArgumentList "/r /t 5 /f" -Wait
+exit 0
